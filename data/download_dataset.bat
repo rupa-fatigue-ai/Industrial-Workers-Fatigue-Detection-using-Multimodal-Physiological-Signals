5b02ec3cc21468bc5c658f85bfe1705b44be2b49
rupa-fatigue-ai <rupajh099@gmail.com>
2026-04-18 20:47:14 +0530

    Add files via upload

---
+++ data/download_dataset.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Opens the Google Drive folder in default browser
+start "" "https://drive.google.com/drive/folders/133d4LgHQ6PHoftxWmM_0G7kPBhQPWX3R?usp=drive_link"
---
+++ data/download_dataset_macOS.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Opens the Google Drive folder in default browser
+open "https://drive.google.com/drive/folders/133d4LgHQ6PHoftxWmM_0G7kPBhQPWX3R?usp=drive_link"
